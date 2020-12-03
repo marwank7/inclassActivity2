e619f52593ad3f71366c8a61dcf1ab1d75417cb2
marwan <marwankouba98@gmail.com>
2020-12-03 21:11:23 +0200

    Version 1.0

---
+++ app/src/main/java/com/example/inclassactivity2/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.inclassactivity2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        String url = "http://127.0.0.1:8080/inclassAct/info.php";
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET},
+                    123);
+
+        } else{
+            DownloadTextTask runner = new DownloadTextTask();
+            runner.execute(url);
+        }
+
+    }
+    private InputStream OpenHttpConnection(String urlString) throws IOException
+    {
+        InputStream in = null;
+        int response = -1;
+
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+
+        if (!(conn instanceof HttpURLConnection))
+            throw new IOException("Not an HTTP connection");
+        try{
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            response = httpConn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.d("Networking", ex.getLocalizedMessage());
+            throw new IOException("Error connecting");
+        }
+        return in;
+    }
+    private String DownloadText(String URL)
+    {
+        int BUFFER_SIZE = 2000;
+        InputStream in = null;
+        try {
+            in = OpenHttpConnection(URL);
+        } catch (IOException e) {
+            Log.d("Networking", e.getLocalizedMessage());
+            return "";
+        }
+
+        InputStreamReader isr = new InputStreamReader(in);
+        int charRead;
+        String str = "";
+        char[] inputBuffer = new char[BUFFER_SIZE];
+        try {
+            while ((charRead = isr.read(inputBuffer))>0) {
+                //---convert the chars to a String---
+                String readString =
+                        String.copyValueOf(inputBuffer, 0, charRead);
+                str += readString;
+                inputBuffer = new char[BUFFER_SIZE];
+            }
+            in.close();
+        } catch (IOException e) {
+            Log.d("Networking", e.getLocalizedMessage());
+            return "";
+        }
+        return str;
+    }
+
+
+    private class DownloadTextTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            return DownloadText(urls[0]);
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            final List< String > ListElementsArrayList = new ArrayList< String >
+                    (Arrays.asList(result));
+
+            ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, ListElementsArrayList);
+            ListView list = findViewById(R.id.list);
+
+            list.setAdapter(listAdapter);
+
+        }
+    }
+    }
